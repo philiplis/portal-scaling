6f00838c8ec1e5dc45652545663dc2211b40c553
philiplis <philiplis@yahoo.co.uk>
2024-08-04 12:41:21 +0100

    New level and assets
    New level added, pretty basic with small room and a corridor that leads you to a much larger, also empty, room.
    Imported some assets to use from the unity asset store (Standard Unity Asset Store EULA)
    Some movement changes also

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,129 +1,4 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//[RequireComponent(typeof(Rigidbody))]
-//public class CameraMove : MonoBehaviour
-//{
-//    private const float moveSpeed = 7.5f;
-//    private const float cameraSpeed = 3.0f;
-
-//    public Quaternion TargetRotation { private set; get; }
-
-//    private Vector3 moveVector = Vector3.zero;
-//    private float moveY = 0.0f;
-
-//    private new Rigidbody rigidbody;
-
-//    private void Awake()
-//    {
-//        rigidbody = GetComponent<Rigidbody>();
-//        Cursor.lockState = CursorLockMode.Locked;
-
-//        TargetRotation = transform.rotation;
-//    }
-
-//    private void Update()
-//    {
-//        // Rotate the camera.
-//        var rotation = new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
-//        var targetEuler = TargetRotation.eulerAngles + (Vector3)rotation * cameraSpeed;
-//        if(targetEuler.x > 180.0f)
-//        {
-//            targetEuler.x -= 360.0f;
-//        }
-//        targetEuler.x = Mathf.Clamp(targetEuler.x, -75.0f, 75.0f);
-//        TargetRotation = Quaternion.Euler(targetEuler);
-
-//        transform.rotation = Quaternion.Slerp(transform.rotation, TargetRotation, 
-//            Time.deltaTime * 15.0f);
-
-//        // Move the camera.
-//        float x = Input.GetAxis("Horizontal");
-//        float z = Input.GetAxis("Vertical");
-//        moveVector = new Vector3(x, 0.0f, z) * moveSpeed;
-
-//        moveY = Input.GetAxis("Elevation");
-//    }
-
-//    private void FixedUpdate()
-//    {
-//        Vector3 newVelocity = transform.TransformDirection(moveVector);
-//        newVelocity.y += moveY * moveSpeed;
-//        rigidbody.velocity = newVelocity;
-//    }
-
-//    public void ResetTargetRotation()
-//    {
-//        TargetRotation = Quaternion.LookRotation(transform.forward, Vector3.up);
-//    }
-//}
-
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//[RequireComponent(typeof(Rigidbody))]
-//public class CameraMove : MonoBehaviour
-//{
-//    private const float moveSpeed = 7.5f;
-//    private const float cameraSpeed = 3.0f;
-
-//    public float gravity = 9.8f;
-//    public float jumpHeight = 2.0f;
-
-//    public Quaternion TargetRotation { private set; get; }
-
-//    private Vector3 moveVector = Vector3.zero;
-//    private float moveY = 0.0f;
-
-//    private new Rigidbody rigidbody;
-
-//    private void Awake()
-//    {
-//        rigidbody = GetComponent<Rigidbody>();
-//        Cursor.lockState = CursorLockMode.Locked;
-
-//        TargetRotation = transform.rotation;
-//    }
-
-//    private void Update()
-//    {
-//        // Rotate the camera.
-//        var rotation = new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
-//        var targetEuler = TargetRotation.eulerAngles + (Vector3)rotation * cameraSpeed;
-//        if (targetEuler.x > 180.0f)
-//        {
-//            targetEuler.x -= 360.0f;
-//        }
-//        targetEuler.x = Mathf.Clamp(targetEuler.x, -75.0f, 75.0f);
-//        TargetRotation = Quaternion.Euler(targetEuler);
-
-//        transform.rotation = Quaternion.Slerp(transform.rotation, TargetRotation,
-//            Time.deltaTime * 15.0f);
-
-//        // Move the camera.
-//        float x = Input.GetAxis("Horizontal");
-//        float z = Input.GetAxis("Vertical");
-//        moveVector = new Vector3(x, 0.0f, z) * moveSpeed;
-
-//        moveY = Input.GetAxis("Elevation");
-//    }
-
-//    private void FixedUpdate()
-//    {
-//        Vector3 newVelocity = transform.TransformDirection(moveVector);
-//        newVelocity.y += moveY * moveSpeed;
-//        rigidbody.velocity = newVelocity;
-//    }
-
-//    public void ResetTargetRotation()
-//    {
-//        TargetRotation = Quaternion.LookRotation(transform.forward, Vector3.up);
-//    }
-//}
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -134,14 +9,12 @@ public class CameraMove : MonoBehaviour
 {
     public float cameraSpeed = 2.0f;
     public float moveSpeed = 5.0f;
-    public float gravity = 9.8f;
     public float jumpHeight = 1.5f;
 
     public UnityEngine.Quaternion TargetRotation { get; private set; }
 
     Rigidbody rb;
     private UnityEngine.Vector3 moveVector = UnityEngine.Vector3.zero;
-
 
     float horizontalMovement;
     float verticalMovement;
@@ -159,33 +32,12 @@ public class CameraMove : MonoBehaviour
 
     private void Update()
     {
-
-
-        // Move the camera.
-        //float x = Input.GetAxis("Horizontal");
-        //float z = Input.GetAxis("Vertical");
-        //moveVector = new Vector3(x, 0.0f, z) * moveSpeed;
         if (!PauseController.gameIsPaused)
         {
             PlayerInput();
         }
-
-        // Jumping logic
-        
-        //if (IsGrounded())
-        //{
-        //    verticalVelocity = -gravity * Time.deltaTime;
-
-        //    if (Input.GetButtonDown("Jump"))
-        //    {
-        //        verticalVelocity = Mathf.Sqrt(jumpHeight * 2 * gravity);
-        //    }
-        //}
-        //else
-        //{
-        //    verticalVelocity -= gravity * Time.deltaTime;
-        //}
     }
+
     void PlayerInput()
     {
         // Rotate the camera.
@@ -195,46 +47,31 @@ public class CameraMove : MonoBehaviour
         {
             targetEuler.x -= 360.0f;
         }
-        targetEuler.x = Mathf.Clamp(targetEuler.x, -75.0f, 75.0f);
+        targetEuler.x = Mathf.Clamp(targetEuler.x, -90.0f, 89.0f);
         TargetRotation = UnityEngine.Quaternion.Euler(targetEuler);
 
         transform.rotation = UnityEngine.Quaternion.Slerp(transform.rotation, TargetRotation,
             Time.deltaTime * 15.0f);
 
-        //Player Movement
+        // Player Movement
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
         moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
 
-        //Player jump
-        if ( Input.GetButtonDown("Jump") && IsGrounded())
+        // Player jump
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            //force.y = 100 * transform.parent.transform.localScale[0]
-            rb.AddForce(new Vector3(0, 20 * jumpHeight, 0), ForceMode.Impulse);
-
+            rb.AddForce(new Vector3(0, Mathf.Sqrt(2 * jumpHeight * Physics.gravity.magnitude), 0), ForceMode.VelocityChange);
         }
-        
     }
+
     private void FixedUpdate()
     {
-        Vector3 force = moveDirection.normalized * moveSpeed * 10f;
-        force.y = -9.8f;
-        rb.AddForce(force, ForceMode.Acceleration);
-    }
-    //private void FixedUpdate()
-    //{
-        
-    //    //moveVector[1] 
-    //    UnityEngine.Vector3 newVelocity = transform.TransformDirection(moveVector);
-    //    //newVelocity.y = verticalVelocity;
+        Vector3 horizontalForce = moveDirection.normalized * moveSpeed;
+        Vector3 totalForce = new Vector3(horizontalForce.x, rb.velocity.y, horizontalForce.z);
 
-    //    //characterController.Move(newVelocity * Time.deltaTime);
-    //    rb.velocity = newVelocity;
-    //    if (Input.GetButtonDown("Jump"))
-    //    {
-    //        rb.AddForce(new Vector3(0, 15 * jumpHeight, 0), ForceMode.Impulse);
-    //    }
-    //}
+        rb.velocity = totalForce;
+    }
 
     public void ResetTargetRotation()
     {
@@ -243,6 +80,6 @@ public class CameraMove : MonoBehaviour
 
     bool IsGrounded()
     {
-        return (GetComponentInParent<Rigidbody>().velocity.y == 0) ;
+        return Physics.Raycast(transform.position, Vector3.down, 1.5f); //TODO Multiply by Scale factor
     }
 }
